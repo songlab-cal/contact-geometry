aa9682b9f5d82890ee84252ea98f6205c7fef81f
Milind Jagota <milind_jagota@berkeley.edu>
2021-07-31 14:12:08 -0700

    Update probe_all.py

--- self_contacts/probe_all.py
+++ self_contacts/probe_all.py
@@ -6,6 +6,6 @@ pdb_chains = open('pdb_lists/abb8330_Data_S1.txt').read().splitlines()
 pdb_chains = [line for line in pdb_chains if line[0] != '#']
 
 for pdb_code in tqdm(pdb_codes):
-    chains = [line[-2] for line in pdb_chains if line[:4] == pdb_code]
+    chains = [line[4] for line in pdb_chains if line[:4] == pdb_code]
     pdb_file = 'data/reduce_out/reduce_' + pdb_code + '.pdb'
     probe_extraction.parse_probe(pdb_file, chains, outdir='data/probe_out')
