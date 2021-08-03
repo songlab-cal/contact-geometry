3e45373ec4fd9e55c5fb5834bd677bc9c7c2ec89
Antoine Koehl <antoinekoehl@gmail.com>
2021-08-02 19:53:08 -0700

    probe, restrictive

--- protein_ligand_contacts/contact_extraction/extract_vdms.py
+++ protein_ligand_contacts/contact_extraction/extract_vdms.py
@@ -10,13 +10,15 @@ import numpy as np
 import pandas as pd
 
 from schrodinger import structure
-from schrodinger.structutils import analyze
+from schrodinger.structutils import analyze, rings
 from ifg import VDM
+from functional_groups import ALPHABET
 
 xyxcoord = Tuple[float, float, float]
 
-VDM_SMARTS = ['[C,c]=O', 'CC(=O)[O-]','CC(=O)[OH]', 'CC(C)[C;!R]', '[C,c]C([N;H2,H1])=O', '[C,c][N;H2,H1]', '[C,c][O;H1]', '[CX4;H2][S;H1,H0]', 'CSC', 'N=C(N)N', '[O;H1]c1ccccc1', '[C,c][N+;H3]', 'c1c[nH]c2ccccc12', 'c1c[n;H0,H1]cn1', 'c1ccccc1', '[C,c][C;!R]([C;!R])[O;H1]', '[C,c][C;H2;!R][O;H1]', 'c[n;H0,H1]c', '[$([NX3H,NX4H2+]),$([NX3](C)(C)(C))]1[CX4H]([CH2][CH2][CH2]1)','[CHX4]([CH3X4])[CH3X4]', '[CHX4]([CH3X4])[CH2X4][CH3X4]']
+VDM_SMARTS = ['[C,c]=O', 'CC(=O)[O-]','CC(=O)[OH]', 'CC(C)[C;!R]', '[C,c]C(=O)[N;H2,H1]', '[C,c][N;H2,H1]', '[C,c][O;H1]', '[CX4;H2][S;H1,H0]', 'CSC', 'N=C(N)N', '[O;H1]c1ccccc1', '[C,c][N+;H3]', 'c1c[nH]c2ccccc12', 'c1c[n;H0,H1]cn1', 'c1ccccc1', '[C,c][C;!R]([C;!R])[O;H1]', '[C,c][C;H2;!R][O;H1]', 'c[n;H0,H1]c', '[$([NX3H,NX4H2+]),$([NX3](C)(C)(C))]1[CX4H]([CH2][CH2][CH2]1)','[CHX4]([CH3X4])[CH3X4]', '[CHX4]([CH3X4])[CH2X4][CH3X4]']
 VDM_NAMES = ['carbonyl', 'carboxylate', 'carboxylate', 'isopropyl', 'carboxamide', 'NH', 'OH', 'thiol', 'thioether', 'guanidine', 'phenol', 'posN', 'indole', 'imidazole', 'phenyl', 'isopropanol', 'ethanol', 'secondary_amine', 'pro_ring', 'val_side', 'ile_side']
+
 
 def _all_ifgs(ligand) -> Tuple[list, list]:
     '''
@@ -46,17 +48,45 @@ def _all_ifgs(ligand) -> Tuple[list, list]:
     
     return substruc_matches, substruc_names
 
-def _unique_ifgs(all_matches: List[list], all_names: List[str]) -> Dict[str, list]:
+def _all_ifgs_restrained(ligand):
+
+    VDM_SMARTS_2 = ['[C,c]=O', '[C,c][CX3](=[OX1])[OH0-,OH]', 'CC(C)[C;!R]', '[C,c][CX3](=[OX1])[NX3H2]','[C,c][N;H2,H1]', '[C,c][O;H1]', '[CX4;H2][S;H1,H0]', 'CSC', 'N=C(N)N', '[O;H1]c1ccccc1', '[C,c][N+;H3]', 'c1c[nH]c2ccccc12', 'c1c[n;H0,H1]cn1', 'c1ccccc1', '[C,c][C;!R]([C;!R])[O;H1]', '[C,c][C;H2;!R][O;H1]', 'c[n;H0,H1]c', '[$([NX3H,NX4H2+]),$([NX3](C)(C)(C))]1[CX4H]([CH2][CH2][CH2]1)','[CHX4]([CH3X4])[CH3X4]', '[CHX4]([CH3X4])[CH2X4][CH3X4]']
+    VDM_NAMES_2 = ['carbonyl', 'carboxylate', 'isopropyl', 'carboxamide', 'NH', 'OH', 'thiol', 'thioether', 'guanidine', 'phenol', 'posN', 'indole', 'imidazole', 'phenyl', 'isopropanol', 'ethanol', 'secondary_amine', 'pro_ring', 'val_side', 'ile_side']
+
+    ring_vdms = ['phenol', 'phenyl', 'indole', 'imidazole']
+    substruc_matches = list()
+    substruc_names = list()
+
+    #lists of ring systems. Two rings are merged into a ring system if they share a bond in common
+    ring_sys = rings.find_ring_systems(ligand)
+
+    for i, vdm in enumerate(VDM_SMARTS_2):
+        name = VDM_NAMES_2[i]
+        substructs = analyze.evaluate_smarts_canvas(ligand, smarts = vdm, start_index = 1, uniqueFilter=True)
+
+        if substructs:
+            for match in substructs:
+                #avoid including ring vdms if they are part of a ring system
+                if name in ring_vdms and any([set(match) < set(r) for r in ring_sys]):
+                    continue
+                
+                substruc_matches.append([ligand.atom[a].pdbname.strip() for a in match])
+                substruc_names.append(name)
+    
+    return substruc_matches, substruc_names
+
+def _unique_ifgs(all_matches: List[list], all_names: List[str]) -> Tuple[list, list]:
     '''
-    Returns unique iFGs from a list of all matches. Any iFG that is fully contained in another vdM
+    Returns unique vdMs from a list of all matches. Any vdM that is fully contained in another vdM
     in the list is removed.
 
     Args:
-    all_matches - all iFGs in ligand
-    all_names - all atom names in ligand
+    all_matches - all vdms in ligand
+    all_names - all names
 
     Returns:
-    vdm_dict - dictionary indexed by iFG names returning the corresponding atoms that make up that iFG
+    final_matches - all unique vdM instances
+    final_names - corresponding names
     '''
 
     final_matches = []
@@ -77,19 +107,7 @@ def _unique_ifgs(all_matches: List[list], all_names: List[str]) -> Dict[str, lis
 
     return vdm_dict
 
-def parse_structure_file(filename: Union[str, PathLike]) -> Tuple[dict, dict]:
-
-    '''
-    Parses a Maestro file to get information about atoms in the structure, and extracts the iFGs from the ligand.
-
-    Args:
-    filename - input filename (.mae). Ideally this file has been preprocessed by the protein preparation wizard
-
-    Returns:
-    atomdict - Dictionary of all atoms and their coordinates in the structure, indexed by residue in the following format:
-                CH:RESNUM(RESNAME)
-    vdm - dictionary of each unique iFG and the ligand atoms that make it up
-    '''
+def parse_structure_file(filename: Union[str, PathLike], restrictive: bool = False) -> Tuple[dict, dict]:
 
     infile = Path(filename)
     res_info_file = infile.with_suffix('.json')
@@ -102,40 +120,41 @@ def parse_structure_file(filename: Union[str, PathLike]) -> Tuple[dict, dict]:
     atomdict = dict()
 
     for at in a_info:
+        if at[2].strip() in ALPHABET:
+            pdbres = ALPHABET[at[2].strip()]
+        else:
+            pdbres = at[2].strip()
         idx = at[0] 
         if at[6] != ' ': 
-            resid = f"{at[1]}:{at[3]}{at[6]}({at[2].strip()})" 
+            resid = f"{at[1]}:{at[3]}{at[6]}({pdbres})" 
         else: 
-            resid = f"{at[1]}:{at[3]}({at[2].strip()})" 
+            resid = f"{at[1]}:{at[3]}({pdbres})" 
         at_name = at[4].strip() 
         coords = at[5] 
         if resid not in atomdict: 
             atomdict[resid] = {at_name: {'index':idx, 'coords': coords}} 
         else: 
             atomdict[resid].update({at_name: {'index': idx, 'coords': coords}})
-    #save processed data
+
     with open(res_info_file, 'w+') as fout:
         json.dump(atomdict, fout)
-    #ligand chain object will be Chain L following our preprocessing
+
     ligchain = cplx.chain['L']
     ligst = ligchain.extractStructure()
 
-    m,n = _all_ifgs(ligst)
+    if not restrictive:
+        m,n = _all_ifgs(ligst)
+    else:
+        m, n = _all_ifgs_restrained(ligst)
     vdm = _unique_ifgs(m,n)
 
-    return atomdict, vdm
+    #gets mapping from ligand hydrogen to heavy atom for better parsing probe
+    #each list only has one entry, so take 0 element
+    protonmap = {atom.pdbname.strip():[a.pdbname.strip() for a in atom.bonded_atoms][0] for atom in ligst.atom if atom.element == 'H'}
 
-def parse_poseviewer_file(poseviewer_file: Union[str, PathLike]) -> Dict[str, List[Tuple[str, str]]]:
-    '''
-    Parses the output of SCRODINGER's poseviwer_interactions.py
+    return atomdict, vdm, protonmap
 
-    Args:
-    poseviewer_file - csv file output from poseviewer_interactions.py, requires the "-csv" flag when running
-
-    Returns:
-    Dictionary of interactions broken down by ligand. Each entry is a ligand atom, and the values are lists of tuples of 
-    receptor atom, and type of interaction as identified by the poseviewer_interactions script
-    '''
+def parse_poseviewer_file(poseviewer_file: Union[str, PathLike]) -> dict:
 
     lig_interactions = dict()
 
@@ -152,8 +171,15 @@ def parse_poseviewer_file(poseviewer_file: Union[str, PathLike]) -> Dict[str, Li
 
             ra = ligdf['RecAtom'].iloc[i]
             ratom = ra[ra.find('(')+1 : ra.find(')')].strip()
+            
+            rec_res = ligdf['RecResidue'].iloc[i].strip()
+            resid = rec_res[rec_res.find('(') +1 : rec_res.find(')')].strip()
+            other_info = rec_res[0:rec_res.find('(')]
 
-            rr = ligdf['RecResidue'].iloc[i].strip()
+            if resid in ALPHABET:
+                rr = f"{other_info}({ALPHABET[resid]})"
+            else:
+                rr = rec_res
 
             interaction_type = ligdf.Type.iloc[i]
             if interaction_type.startswith('HDonor') or interaction_type.startswith('HAccep') or interaction_type == 'Ar-Hbond':
@@ -173,17 +199,79 @@ def parse_poseviewer_file(poseviewer_file: Union[str, PathLike]) -> Dict[str, Li
     
     return lig_interactions
 
-def gen_vdms(vdm_dict: Dict[str, str], atom_dict: dict) -> dict:
+def parse_probe_line(line: str) -> tuple:
     '''
-    Initializes vdM objects from the iFGs and atomic coordinates extracted by parse_structure_file
-
+    Parses a probe line to extract salient information about 'hb' or 'cc' contacts to the ligand
+    (chain L)
+    
     Args:
-    vdm_dict - dictionary of iFGs and their corresponding atoms
-    atom_dict - dictionary of atomic coordinates and indices, indexed first by residue
+    line (str) - line of output from probe
 
     Returns:
-    Dictionary with a single Ligand (for this demonstration) that yields a list of iFGs
+    tuple of (interaction type, chain1, resnum1, resname1, atom name1, chain2, resnum2, resname2, atom name2) where 1 and 2
+    are the interacting pair
     '''
+
+    spl = line.split(':')[1:]
+    INTERACTION = spl[1]
+    CHAIN1 = spl[2][:2].strip()
+    RESNUM1 = int(spl[2][2:6])
+    RESNAME1 = spl[2][6:10].strip()
+    NAME1 = spl[2][10:15].strip()
+    CHAIN2 = spl[3][:2].strip()
+    RESNUM2 = int(spl[3][2:6])
+    RESNAME2 = spl[3][6:10].strip()
+    NAME2 = spl[3][10:15].strip()
+    #deal with waters
+    if RESNAME1 == 'HOH':
+        NAME1 = 'O'
+    if RESNAME2 == 'HOH':
+        NAME2 = 'O'
+
+    return (INTERACTION, CHAIN1, RESNUM1, RESNAME1, NAME1, CHAIN2, RESNUM2, RESNAME2, NAME2)
+
+def parse_probe_file(probe_file: Union[str, Path], Hmap: Dict[str, str]) -> dict:
+    '''
+    Parse the output from probe into a dictionary of interactions keyed by the ligand atoms
+
+    Args:
+    probe_file - the probe file name
+    Hmap - mapping between ligand Hydrogens and their heavy atoms
+    Returns:
+    dictionary of interactions between the ligand and protein.
+    '''
+    probe_dict = {'Ligand_1' : {}}
+
+    with open(probe_file, 'r+') as fin:
+        for line in fin:
+            interact, ch1, resnum1, resname1, name1, ch2, resnum2, resname2, name2 = parse_probe_line(line)
+            if ch1 != 'L' or (ch1 == 'L' and ch2 == 'L'):
+                continue
+            else:
+                if name1.startswith('H') and name1 in Hmap:
+                    #proton, assign it to its bonded heavy atom
+                    l_atom = Hmap[name1]
+                else:
+                    l_atom = name1
+
+                rr = f"{ch2}:{resnum2}({resname2})"
+                ratom = name2
+
+                if interact == 'hb' or interact == 'cc':
+
+                    int_type = 'hbond' if interact == 'hb' else 'vdw'
+
+                    if l_atom not in probe_dict["Ligand_1"]:
+                        probe_dict["Ligand_1"][l_atom] = dict()
+
+                    if rr not in probe_dict["Ligand_1"][l_atom]:
+                        probe_dict["Ligand_1"][l_atom][rr] = list()
+                    
+                    probe_dict["Ligand_1"][l_atom][rr].append((ratom, int_type))
+
+    return probe_dict
+
+def gen_vdms(vdm_dict: Dict[str, str], atom_dict: dict) -> dict:
 
     vdm_dictionary = dict()
 
@@ -197,24 +285,13 @@ def gen_vdms(vdm_dict: Dict[str, str], atom_dict: dict) -> dict:
                 #atoms = [lig_info[a] for a in vdm_dict[ifg]]
                 atoms = vdm_dict[ifg]
                 coords = [lig_info[a]['coords'] for a in vdm_dict[ifg]]
-                #instantiate the VDM object with name and coordinates
                 vdm = VDM(ifg, atoms, coords)
 
                 vdm_dictionary['Ligand_1'].append(vdm)
     
     return vdm_dictionary
 
-def get_residue_info(res:str, atom_dict:dict) -> Tuple[list, list]:
-    '''
-    Returns atoms and coordinates for a given residue as two lists
-
-    Args:
-    res - residue name in format CH:RESNUM(RESNAME) as established in parse_structure_file
-    atom_dict - dictionary of atomic coordinates and indices, indexed first by residue
-
-    Returns:
-    list of atoms, list of coordinates for that residue
-    '''
+def get_residue_info(res, atom_dict):
 
     atoms = []
     coords = []
@@ -225,17 +302,7 @@ def get_residue_info(res:str, atom_dict:dict) -> Tuple[list, list]:
     
     return atoms, coords
 
-def parse_contact(contact: List[Tuple[str, str]], atom_dict: Dict[str, dict]) -> dict:
-    '''
-    Parses a set of contacts for a given ligand atom
-
-    Args:
-    contact - list of contacts to a given atom (of the form (atom, contact type))
-    atom_dict - dictionary of atomic coordinates and indices, indexed first by residue
-
-    Returns:
-    Dictionary of contacts for the ligand atom
-    '''
+def parse_contact(contact, atom_dict):
 
     contact_dict = dict()
 
@@ -262,18 +329,6 @@ def parse_contact(contact: List[Tuple[str, str]], atom_dict: Dict[str, dict]) ->
 
 def populate_vdms(vdm_dict, atom_dict, interaction_dict) -> None:
 
-    '''
-    Populate contact information for the vdMs identified from the ligand
-
-    Args:
-    vdm_dict - dictionary of vdM objects from gen_vdMs
-    atom_dict - dictionary of atomic coordinates and indices, indexed first by residue
-    interaction_dict - dictionary of interactions indexed by ligand atom
-
-    Returns:
-    None - updates the contacts for each vdM
-    '''
-
     for lig in vdm_dict:
 
         lig_interactions = interaction_dict[lig]
@@ -286,19 +341,7 @@ def populate_vdms(vdm_dict, atom_dict, interaction_dict) -> None:
 
             vdm.update_contacts(r)
 
-def generate_all_vdms(base_dir: str) -> None:
-
-    '''
-    Generates vdMs using a pair of preprocessed maestro and poseviewer interaction files
-    vdms are saved as pdb_name + '_vdms.pkl). ie if the pdb file was originally 4DKL, the processed maestro file is 4DKL_proc_out.mae
-    and the vdm file will be 4DKL_vdms.pkl
-
-    Args:
-    Base directory with subdirectories for each PDB file. 
-
-    Returns:
-    None - generates and saves vdMs
-    '''
+def generate_all_vdms(base_dir: str, restrictive: bool = False, read_probe: bool =False):
 
     for dir in glob.glob(base_dir + '*'):
 
@@ -307,19 +350,36 @@ def generate_all_vdms(base_dir: str) -> None:
 
         print(f"Working on {pdb_id}")
 
-        maefile = d / (pdb_id + '_proc_out.mae')
-        pv_file = d / (pdb_id +'_proc_out_pv_interactions.csv')
+        maefile = d / (pdb_id + '_out.mae')
+        pv_file = d / (pdb_id +'_out_pv_interactions.csv')
+        probe_file = maefile.with_suffix('.probe')
 
-        atomd, vdms = parse_structure_file(maefile)
-        interactiond = parse_poseviewer_file(pv_file)
+        atomd, vdms, protonmap= parse_structure_file(maefile, restrictive)
+
+        if not read_probe:
+            interactiond = parse_poseviewer_file(pv_file)
+        else:
+            interactiond = parse_probe_file(probe_file, protonmap)
+
+        if not interactiond:
+            print(f"Error with {pdb_id}")
+            continue
 
         vdmd = gen_vdms(vdms, atomd)
 
         populate_vdms(vdmd, atomd, interactiond)
 
-        vdm_file = d / (pdb_id +'_vdms.pkl')
+        if not restrictive:
+            vdm_file = d / (pdb_id +'_vdms.pkl')
+        else:
+            vdm_file = d / (pdb_id + '_restrictive_vdms.pkl')
 
-        with open(vdm_file, 'wb') as fout:
+        if read_probe:
+            outfile = vdm_file.parent / (vdm_file.stem + '_probe_extracted.pkl')
+        else:
+            outfile = vdm_file
+        
+        with open(outfile, 'wb') as fout:
             pickle.dump(vdmd, fout)
 
 if __name__ == "__main__":
@@ -327,7 +387,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process Maestro Files To Extract VDMS")
 
     parser.add_argument("-i", "--input_dir", type=str, help="Input Directory")
+    parser.add_argument("-restrictive", default = False, action= 'store_true', help= "Make iFG definitions more restrictive (strictly like they appear on side chains")
+    parser.add_argument("-probe", default=False, action= 'store_true', help = "Parse interactions from probe")
 
     args = parser.parse_args()
 
-    generate_all_vdms(args.input_dir)
+    if args.restrictive:
+        strict = True
+    else:
+        strict = False
+
+    if args.probe:
+        use_probe = True
+    else:
+        use_probe = False
+
+    generate_all_vdms(args.input_dir, strict, use_probe)
